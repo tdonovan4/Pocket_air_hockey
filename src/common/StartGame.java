f081705c66606637c433c903444f947a05a9dae2
tdonovan4 <tdonovan@codengineer.com>
2016-04-20 19:37:57 -0400

    Préparation render des formes

--- src/common/StartGame.java
+++ src/common/StartGame.java
@@ -1,8 +1,14 @@
 package common;
 
+import frame.GameFrame;
+
 public class StartGame {
+	
+	GameFrame gFrame = new GameFrame();
+	
 	public void start() {
 		System.out.println("Start!");
 		//go!
+		gFrame.paintComponent(null);
 	}
 }
--- src/frame/GameFrame.java
+++ src/frame/GameFrame.java
@@ -1,5 +1,9 @@
 package frame;
 
+import java.awt.Graphics;
+
 public class GameFrame {
-	
+	public void paintComponent(Graphics g) {
+		 System.out.println("Test!");
+	}
 }
